2d140a2fa5552040b022d3dc0f98992cb933173e
YunminCompaney <chm46650@gmail.com>
2025-03-17 13:42:06 +0900

    코드 수정

--- main/public/problems/javascript/js_files/num1.js
+++ main/public/problems/javascript/js_files/num1.js
@@ -2,5 +2,7 @@ const foo = [1, 2, 3, 4];
 const bar = 12;
 
 if ((foo[3] + foo[4]) <= bar) {
-	const result = 'correct!';
+    const result = 'correct!';
 }
+
+console.log(`${result}!`); // correct!
